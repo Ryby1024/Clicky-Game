718557bb3166e583d255e0c66d6f1336d2244038
Roy Davis <myroy1024@yahoo.com>
2020-02-14 16:29:20 -0500

    Updates

---
+++ precache-manifest.92867dadb79b403745288e2dacd4f3b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f904bd609df6b2987b03faa02a425dc",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9b2940d1f2a776fcdabf",
+    "url": "/Clicky-Game/static/css/main.a848d2c1.chunk.css"
+  },
+  {
+    "revision": "9d0ae5715d14ab333d0b",
+    "url": "/Clicky-Game/static/js/2.8f65b5a5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.8f65b5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2940d1f2a776fcdabf",
+    "url": "/Clicky-Game/static/js/main.8e7869cb.chunk.js"
+  },
+  {
+    "revision": "331e595879dc7af9f8f0",
+    "url": "/Clicky-Game/static/js/runtime-main.e5d1295f.js"
+  }
+]);
